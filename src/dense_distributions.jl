8754b238c515853954dc9642ec96e04d8d0e478b
dpmerrell <davidpmerrell@gmail.com>
2020-10-12 21:32:42 -0500

    Rearranged things. Started Snakefile and config.

--- src/dense_distributions.jl
+++ src/dense_distributions.jl
@@ -6,6 +6,7 @@
 #
 
 using Gen
+import Gen.logpdf, Gen.logpdf_grad, Gen.random
 using StatsFuns
 using LinearAlgebra
 import Distributions: Chisq
@@ -14,19 +15,20 @@ import Distributions: Chisq
 # Cholesky factor
 struct CholPrecMVN <: Gen.Distribution{Vector{Float64}} end
 
-const chol_prec_mvn = SparseCholPrecMVN()
+const chol_prec_mvn = CholPrecMVN()
 
-chol_prec_mvn(chol_prec::Matrix{Float64}) = random(chol_prec_mvn, chol_prec)
-
-function random(cpm::CholPrecMVN, chol_prec_tril::Matrix{Float64})
+function Gen.random(cpm::CholPrecMVN, chol_prec_tril::LowerTriangular{Float64,Matrix{Float64}})
     N = size(chol_prec_tril,1) 
     z = randn(N)
     return transpose(chol_prec_tril) \ z 
 end
 
+chol_prec_mvn(chol_prec::LowerTriangular{Float64, Matrix{Float64}}) = Gen.random(chol_prec_mvn, chol_prec)
+
+L2PI = log(2.0*pi)
 
 function logpdf(cpm::CholPrecMVN, x::Vector{Float64}, 
-		chol_prec_tril::Matrix{Float64})
+		chol_prec_tril::LowerTriangular{Float64, Matrix{Float64}})
     z = transpose(x)*chol_prec_tril
     ztz = z*transpose(z)
     
@@ -37,7 +39,7 @@ end
 
 
 function logpdf_grad(cpm::CholPrecMVN, x::Vector{Float64}, 
-		     chol_prec_tril::Matrix{Float64})
+		     chol_prec_tril::LowerTriangular{Float64,Matrix{Float64}})
 
     x_grad = -transpose(transpose(x)*chol_prec_tril)
     chol_prec_grad = -(reshape(x,(size(x,1),1)) .* chol_prec_tril) + diagm(1.0./diag(chol_prec_tril) )
@@ -47,36 +49,35 @@ function logpdf_grad(cpm::CholPrecMVN, x::Vector{Float64},
 end
 
 
-has_output_grad(chol_prec_mvn) = true
-has_argument_grads(chol_prec_mvn) = true
+Gen.has_output_grad(chol_prec_mvn) = true
+Gen.has_argument_grads(chol_prec_mvn) = true
 
 
 # Wishart distribution (over cholesky factors)
 # parameterized by a Cholesky factor.
 
-struct CholWishart <: Distribution{Matrix{Float64}} end
+struct CholWishart <: Distribution{LowerTriangular{Float64,Matrix{Float64}}} end
 
 const chol_wishart = CholWishart()
-
-chol_prec_mvn(dof::Float64, V_chol::Matrix{Float64}) = random(chol_wishart, dof, V_chol)
 
 """
     Sample a lower triangular matrix L, 
     using the "Bartlett decomposition"
 """
-function random(cw::CholWishart, dof::Float64, V_chol::Matrix{Float64})
+function Gen.random(cw::CholWishart, dof::Float64, V_chol::LowerTriangular{Float64,Matrix{Float64}})
 
     p = size(V_chol,1)
     A = tril(randn(p,p))
-    A[diagind(A)] = [rand(Chisq(dof-i+1)) for i=1:p]
+    A[diagind(A)] = [sqrt(rand(Chisq(dof-i+1))) for i=1:p]
 
-    return L * A
+    return LowerTriangular(V_chol * A)
 
 end
 
+chol_wishart(dof::Float64, V_chol::LowerTriangular{Float64,Matrix{Float64}}) = Gen.random(chol_wishart, dof, V_chol)
 
-function logpdf(cw::CholWishart, omega_chol::Matrix{Float64},
-		dof::Float64, V_chol::Matrix{Float64})
+function logpdf(cw::CholWishart, omega_chol::LowerTriangular{Float64, Matrix{Float64}},
+		dof::Float64, V_chol::LowerTriangular{Float64, Matrix{Float64}})
     # Get dimensionality
     p = size(V_chol,1)
 
@@ -85,10 +86,10 @@ function logpdf(cw::CholWishart, omega_chol::Matrix{Float64},
     lp = sum(logdiag_omega_chol)*(dof - p - 1)
     lp -= sum((V_chol \ omega_chol).^2.0)*0.5
     lp -= p*dof*0.5*log(2.0)
-    lp -= dof*0.25*sum(log.(abs.(diag(V_chol))))
+    lp -= dof*sum(log.(abs.(diag(V_chol))))
     lp -= logmvgamma(p, 0.5*dof)
 
-    # inv log determinant of cholesky transformation
+    # log inv Jacobian of cholesky transformation
     # (adjustment accounts for transformed output -- 
     # this is a distribution over cholesky factors
     lp += p*log(2.0) + dot(p:-1:1, logdiag_omega_chol)
@@ -96,12 +97,21 @@ function logpdf(cw::CholWishart, omega_chol::Matrix{Float64},
 end
 
 
-function logpdf_grad(cw::CholWishart, omega::Matrix{Float64},     
-		     dof::Float64, V_chol::Matrix{Float64})
+function logpdf_grad(cw::CholWishart, omega_chol::LowerTriangular{Float64,Matrix{Float64}}, 
+		     dof::Float64, V_chol::LowerTriangular{Float64,Matrix{Float64}})
 
-    return (omega_grad, nothing, V_chol_grad)
+    p = size(V_chol,1)
+    V_chol_inv = inv(V_chol)
+
+    omega_chol_grad = diagm( ((dof-1.0):-1.0:(dof-p)) ./ diag(omega_chol) )
+    omega_chol_grad += -V_chol_inv
+
+    V_chol_grad = -sum((-V_chol_inv * tril(ones(p,p)) * V_chol_inv ) * omega_chol)
+    V_chol_grad += -dof*sum(log.(abs.(diag(V_chol))))
+    
+    return (omega_chol_grad, nothing, V_chol_grad)
 end
 
-has_output_grad(cw::CholWishart) = true
-has_argument_grads(cw::CholWishart) = (false, true)
+Gen.has_output_grad(cw::CholWishart) = true
+Gen.has_argument_grads(cw::CholWishart) = (false, true)
 
