90f43a167b94a942e14bd822690b7ad9d95336f4
David Merrell <davidpmerrell@gmail.com>
2022-03-27 05:10:33 +0000

    Progress on data simulation

--- src/simulate.jl
+++ src/simulate.jl
@@ -1,15 +1,22 @@
 
 
-
 function simulate_data(pathway_sif_data, 
                        pathway_names::Vector{String},
                        sample_ids::Vector{String}, 
                        sample_conditions::Vector{String},
                        sample_batch_dict::Dict{T,Vector{U}},
                        feature_genes::Vector{String}, 
-                       feature_assays::Vector{T}) where T where U
+                       feature_assays::Vector{T},
+                       assay_moments_dict::Dict;
+                       mu_snr=10.0,
+                       delta_snr=10.0,
+                       logistic_mtv=10.0,
+                       sample_snr=10.0
+                      ) where T where U
 
 
+    # Translate the pathways and batch ids 
+    # into regularization matrices
     model = MultiomicModel(pathway_sif_data, 
                            pathway_names,
                            sample_ids, 
@@ -17,19 +24,43 @@ function simulate_data(pathway_sif_data,
                            sample_batch_dict,
                            feature_genes, 
                            feature_assay)
+    
+    unique_feature_batches = unique(model.matfac.feature_batch_ids)
+    assay_moments_dict[""] = (0.0, 10.0)
+    data_moments_vec = [assay_moments_dict[a] for a in unique_feature_batches]
 
-    matfac = model.matfac
-    theta = BMF.batch_matrix(matfac.theta_values,
-                             matfac.feature_batch_ids,
-                             matfac.sample_batch_ids)
-    log_delta = BMF.batch_matrix(matfac.log_delta_values,
-                                 matfac.feature_batch_ids,
-                                 matfac.sample_batch_ids)
+    # Decompose the means and variances of different column
+    # batches -- compute contributions from model parameters
+    assay_moment_decomp = decompose_all_data_signal(data_moments_vec,
+                                                    model.matfac.feature_batch_ids,
+                                                    model.matfac.feature_losses;
+                                                    mu_snr=mu_snr,
+                                                    delta_snr=delta_snr,
+                                                    theta_snr=theta_snr,
+                                                    logistic_mtv=logistic_mtv,
+                                                    sample_snr=sample_snr)
+    # Assemble vectors of batch moments for each model parameter 
+    logsigma_moments_vec = []
+    mu_moments_vec = []
+    logdelta_moments_vec = []
+    theta_moments_vec = []
+    for a in enumerate(assay_moment_decomp)
+        push!(logsigma_moments_vec, a[1])
+        push!(mu_moments_vec, a[2])
+        push!(logdelta_moments_vec, a[3])
+        push!(theta_moments_vec, a[4])
+    end
 
-    sim_params, D_gpu = BMF.simulate_data(matfac.X_reg, matfac.Y_reg,
-                                          matfac.mu_reg, matfac.log_sigma_reg,
-                                          theta, log_delta, 
-                                          matfac.noise_models)
+    sim_params = simulate_params(model.matfac.X_reg, 
+                                 model.matfac.Y_reg,
+                                 model.matfac.row_batch_ids,
+                                 model.matfac.col_batch_ids,
+                                 logsigma_moments_vec,
+                                 mu_moments_vec,
+                                 logdelta_moments_vec,
+                                 theta_moments_vec)
+
+    D_gpu = BMF.simulate_data(sim_params, model.matfac.noise_models)
 
     M = length(model.sample_ids)
     N = length(model.feature_idx)
@@ -38,7 +69,7 @@ function simulate_data(pathway_sif_data,
     D[:, model.feature_idx] .= view(D_gpu, model.internal_sample_idx, 
                                            model.internal_feature_idx)
    
-    return D
+    return sim_params, D
 end
 
 
