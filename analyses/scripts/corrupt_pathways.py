d1a0940e06f65453057f1bcb0014b1bd8f6fa086
david merrell <davidpmerrell@gmail.com>
2022-09-28 15:01:22 -0500

    Updated graph corruption; Snakefile

--- analyses/scripts/corrupt_pathways.py
+++ analyses/scripts/corrupt_pathways.py
@@ -14,52 +14,70 @@ import json
 import sys
 
 
-
-def update_graph(pwy, all_nodes, node_add, node_remove):
-
-    # Get the pathway nodes and unique edges
-    pwy_nodes = set()
-    unique_edges = set()
+def get_pwy_nodes(pwy):
+    nodes = set()
     for edge in pwy:
-        pwy_nodes.add(edge[0])
-        pwy_nodes.add(edge[2])
+        nodes.add(edge[0])
+        nodes.add(edge[2])
+    return nodes
+
+
+def get_unq_pwy_edges(pwy):
+    unq_edges = set()
+    for edge in pwy:
         if edge[0] <= edge[2]:
-            unique_edges.add((edge[0],edge[2]))
+            unq_edges.add((edge[0],edge[2]))
         else:
-            unique_edges.add((edge[2],edge[0]))
+            unq_edges.add((edge[2],edge[0]))
+    return unq_edges
 
 
-    pwy_node_ls = list(pwy_nodes)
-    
-    # Figure out how many we're adding and removing
-    n_add = int(np.ceil(node_add * len(pwy_nodes)))
-    n_remove = int(np.ceil(node_remove * len(pwy_nodes)))
-
-    # Remove nodes
-    to_remove = set(np.random.choice(pwy_node_ls, n_remove, replace=False))
-    updated_pwy = [edge for edge in pwy if (edge[0] not in to_remove) and (edge[2] not in to_remove)]
-    for rm_node in to_remove:
-        pwy_nodes.remove(rm_node)
-    pwy_node_ls = list(pwy_nodes)
-
-    # Compute the degree of each node
+def get_node_degrees(pwy):
+    unique_edges = get_unq_pwy_edges(pwy)
     degrees = defaultdict(lambda : 0)
     for edge in unique_edges:
         degrees[edge[0]] += 1
         degrees[edge[1]] += 1
-    degree_vec = np.array([degrees[n] for n in pwy_node_ls])
 
-    # Choose nodes to add
-    complement = all_nodes.difference(pwy_nodes)
-    nodes_to_add = set(np.random.choice(list(complement), n_add, replace=False))
+    return degrees
+
+
+def update_graph(pwy, all_nodes, node_add, node_remove):
+
+    # Get the pathway nodes and unique edges
+    pwy_nodes = get_pwy_nodes(pwy)
+    pwy_node_ls = sorted(list(pwy_nodes))
+    complement_ls = sorted(list(all_nodes.difference(pwy_nodes)))
+
+    # Compute the set of nodes to remove
+    n_remove = int(np.ceil(node_remove * len(pwy_nodes)))
+    to_remove = set(np.random.choice(pwy_node_ls, n_remove, replace=False))    
+
+    # Construct updated versions of the pathway and
+    # its nodes, with the specified nodes removed 
+    updated_pwy = [edge for edge in pwy if (edge[0] not in to_remove) and (edge[2] not in to_remove)]
+    updated_node_ls = [node for node in pwy_node_ls if node not in to_remove]
+    n_remaining = len(updated_node_ls)
+
+    # Compute the degree of each remaining node
+    degree_dict = get_node_degrees(updated_pwy)
+    degree_ls = [degree_dict[n] for n in updated_node_ls]
+    degree_vec = np.array(degree_ls).astype(float) + 1e-10
+    p_vec = degree_vec / np.sum(degree_vec)
+    # Compute the set of nodes to add
+    n_add = int(np.ceil(node_add * len(pwy_nodes)))
+    to_add = set(np.random.choice(complement_ls, n_add, replace=False))
 
     # For each new node, add edges to it
-    for new_node in nodes_to_add:
-        n_neighbors = np.random.choice(degree_vec)
-        neighbors = np.random.choice(pwy_node_ls, n_neighbors, replace=False, p=degree_vec/np.sum(degree_vec))
-        for neighbor in neighbors:
-            updated_pwy.append([neighbor, "a>", new_node])
-        pwy_node_ls.append(new_node)
+    for new_node in to_add:
+        # Choose the new node's degree at random
+        n_neighbors = np.random.choice(degree_ls)
+        # Select the new node's neighbors at random from the updated node list, 
+        # weighted by their original degree 
+        neighbor_idx = np.random.choice(n_remaining, n_neighbors, replace=False, p=p_vec)
+        for idx in neighbor_idx:
+            updated_pwy.append([updated_node_ls[idx], "a>", new_node])
+        updated_node_ls.append(new_node)
 
     return updated_pwy
 
@@ -86,6 +104,10 @@ def update_k(true_pwy_dict, all_pwys_dict, k_add, k_remove, considered=500):
         considered = len(all_pwys_dict["names"])
     all_pwys_dict = {"names": all_pwys_dict["names"][:considered],
                      "pathways": all_pwys_dict["pathways"][:considered]}
+
+    true_k = len(true_pwy_dict["pathways"])
+    k_add = round(k_add * true_k)
+    k_remove = round(k_remove * true_k)
 
     # Build a map from pathway names to their indices in the full list
     name_to_idx = {name: idx for idx, name in enumerate(all_pwys_dict["names"])}
@@ -115,8 +137,8 @@ def main():
     args = sys.argv
     true_pwy_json = args[1]
     all_pwys_json = args[2]
-    k_add = int(args[3])
-    k_remove = int(args[4])
+    k_add = float(args[3])
+    k_remove = float(args[4])
     node_add = float(args[5])
     node_remove = float(args[6])
     out_json = args[7]
