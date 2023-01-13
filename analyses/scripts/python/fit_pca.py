60d5e8ff94f696c32b324d2beb072395b94f4849
david merrell <davidpmerrell@gmail.com>
2023-01-12 18:53:41 -0600

    PCA fit and transform scripts

--- analyses/scripts/python/fit_pca.py
+++ analyses/scripts/python/fit_pca.py
@@ -1,5 +1,8 @@
 
 from statsmodels.multivariate.pca import PCA
+import script_util as su
+import numpy as np
+import argparse
 import h5py
 import sys
 
@@ -10,12 +13,20 @@ def load_data(data_hdf, modality="mrnaseq"):
     with h5py.File(data_hdf, "r") as f:
 
         data = f["omic_data"]["data"][:,:].transpose()
-        assays = f["omic_data"]["feature_assays"][:]
+        assays = f["omic_data"]["feature_assays"][:].astype(str)
 
         relevant_cols = (assays == modality)
         X = data[:,relevant_cols]
 
-    return X
+        sample_ids = f["omic_data"]["instances"][:].astype(str)
+        sample_groups = f["omic_data"]["instance_groups"][:].astype(str)
+
+        feature_genes = f["omic_data"]["feature_genes"][:].astype(str)
+        feature_genes = feature_genes[relevant_cols]
+        assays = assays[relevant_cols]
+
+    return X, sample_ids, sample_groups, assays, feature_genes
+
 
 
 def remove_missing(X):
@@ -36,6 +47,25 @@ def remove_missing(X):
     return X_nomissing, row_key, col_key
 
 
+def variance_filter(X, filter_frac=0.5):
+
+    col_vars = np.nanvar(X, axis=0)
+    qtile = np.nanquantile(col_vars, 1 - filter_frac)
+    keep_idx = (col_vars >= qtile)
+
+    return X[:,keep_idx], keep_idx
+   
+
+def standardize_columns(X):
+
+    col_std = np.nanstd(X, axis=0)
+    col_means = np.nanmean(X, axis=0)
+
+    X = (X - col_means.transpose()) / col_std.transpose()
+
+    return X, col_means, col_std 
+
+
 def find_knee(rsquare):
 
     # Find maximum of discrete 2nd derivative
@@ -47,30 +77,86 @@ def find_knee(rsquare):
     return max_idx
 
 
-def transform_data(X, n_components=20):
+def transform_data(X, n_components=None):
 
-    result = PCA(X, standardize=True,
+    result = PCA(X, standardize=False,
+                    demean=False,
+                    normalize=False,
                     ncomp=n_components, 
                     missing="fill-em",
                     max_em_iter=500)
 
     X_trans = result.factors
     rsquare = result.rsquare
+    pcs = result.loadings
 
-    knee_idx = find_knee(rsquare)
-    X_trans = X_trans[:,:knee_idx]
+    if n_components is None:
+        knee_idx = find_knee(rsquare)
+        X_trans = X_trans[:,:knee_idx]
 
-    return X_trans
+    return X_trans, pcs
 
 
 if __name__=="__main__":
 
-    
-    data_hdf = args[1]
-    transformed_hdf = args[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_hdf", help="HDF5 containing tabular data")
+    parser.add_argument("transformed_hdf")
+    parser.add_argument("fitted_pca_hdf")
+    parser.add_argument("--output_dim", help="Use the top-`output_dim` principal components.", type=int)
+    parser.add_argument("--variance_filter", help="Discard the features with least variance, *keeping* this fraction of the features.", type=float, default=0.5)
+    args = parser.parse_args()
 
-    X = load_data(data_hdf)
+    data_hdf = args.data_hdf
+    transformed_hdf = args.transformed_hdf
+    pcs_hdf = args.fitted_pca_hdf
+    output_dim = args.output_dim
+    v_frac = args.variance_filter
+
+    # Load data
+    X, sample_ids, sample_groups, feature_assays, feature_genes = load_data(data_hdf)
+
+    print("X SHAPE:")
+    print(X.shape)
+
+    # Remove empty rows and columns
     X_nomissing, row_key, col_key = remove_missing(X)
+    sample_ids = sample_ids[row_key]
+    sample_groups = sample_groups[row_key]
+    feature_assays = feature_assays[col_key]
+    feature_genes = feature_genes[col_key]
 
+    print("X_NOMISSING SHAPE:")
+    print(X_nomissing.shape)
+
+    # Remove the columns with least variance
+    X_filtered, col_key = variance_filter(X_nomissing, v_frac)
+    feature_assays = feature_assays[col_key]
+    feature_genes = feature_genes[col_key]
     
+    print("X_FILTERED SHAPE:")
+    print(X_filtered.shape)
+
+    # Standardize the remaining columns
+    X_std, mu, sigma = standardize_columns(X_filtered)
+    print("X_STD SHAPE:")
+    print(X_std.shape)
+
+    # Perform PCA
+    X_transformed, pcs = transform_data(X_std, n_components=output_dim)
+
+    # Output the transformed data 
+    with h5py.File(transformed_hdf, "w") as f:
+        su.write_hdf(f, "X", X_transformed)
+        su.write_hdf(f, "instances", sample_ids, is_string=True) 
+        su.write_hdf(f, "instance_groups", sample_groups, is_string=True) 
+
+    # Output the fitted principal components and standardization parameters
+    with h5py.File(pcs_hdf, "w") as f:
+        su.write_hdf(f, "Y", pcs)
+        su.write_hdf(f, "mu", mu)
+        su.write_hdf(f, "sigma", sigma)
+        su.write_hdf(f, "feature_assays", feature_assays, is_string=True) 
+        su.write_hdf(f, "feature_genes", feature_genes, is_string=True) 
+        
 
---
+++ analyses/scripts/python/transform_pca.py
@@ -0,0 +1,115 @@
+
+import script_util as su
+import numpy as np
+import argparse
+
+
+def load_data(data_hdf):
+    Z_test = su.load_hdf(data_hdf, "omic_data/data").transpose()
+    feature_genes = su.load_hdf(data_hdf, "omic_data/feature_genes", dtype=str)
+    feature_assays = su.load_hdf(data_hdf, "omic_data/feature_assays", dtype=str)
+    instances = su.load_hdf(data_hdf, "omic_data/instances", dtype=str)
+    instance_groups = su.load_hdf(data_hdf, "omic_data/instance_groups", dtype=str)
+ 
+    return Z_test, feature_genes, feature_assays, instances, instance_groups
+
+
+def load_model(pc_hdf):
+
+    Y = su.load_hdf(pc_hdf, "Y")
+    mu = su.load_hdf(pc_hdf, "mu")
+    sigma = su.load_hdf(pc_hdf, "sigma")
+
+    feature_genes = su.load_hdf(pc_hdf, "feature_genes", dtype=str)
+    feature_assays = su.load_hdf(pc_hdf, "feature_assays", dtype=str)
+
+    return Y, mu, sigma, feature_genes, feature_assays
+
+
+def select_features(test_genes, test_assays,
+                    model_genes, model_assays):
+    test_keys = ["_".join(pair) for pair in zip(test_genes, test_assays)] 
+    model_keys = ["_".join(pair) for pair in zip(model_genes, model_assays)] 
+
+    test_idx, model_idx = su.keymatch(test_keys, model_keys)
+
+    return test_idx, model_idx
+ 
+
+def pca_transform(Z, Y, lr=1e-4, rel_tol=1e-6, max_iter=1000):
+
+    K, N = Y.shape
+    M = Z.shape[0]
+
+    X = np.zeros(K, M)
+    lss = np.inf
+    i = 0
+
+    # Until convergence...
+    while i < max_iter:
+        new_lss = 0.0
+            
+        # Compute the gradient of squared loss w.r.t. X
+        delta = np.dot(X, Y) - Z
+        grad_X = np.dot(Y, delta.transpose())
+        X -= (lr*grad_X)
+      
+        # Compute the loss 
+        np.square(delta, out=delta) 
+        new_lss += np.sum(delta)
+
+        # Check termination criterion
+        if (lss - new_lss)/lss < rel_tol
+
+        # Update loop variables
+        lss = new_lss
+        i += 1
+        print("Iteration: {}; Loss: {:.2f}".format(i, lss))
+
+    return X
+
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_hdf")
+    parser.add_argument("pc_hdf")
+    parser.add_argument("transformed_data_hdf")
+    args = parser.parse_args()
+
+    data_hdf = args.data_hdf
+    pc_hdf = args.pc_hdf
+    out_hdf = args.transformed_data_hdf
+
+    # Load the data and model parameters
+    Z_test, test_genes, test_assays, sample_ids, sample_groups = load_data(data_hdf)
+    Y, mu, sigma, model_genes, model_assays = load_model(pc_hdf)
+
+    # Find the intersection of features that are (1) in the test data
+    # and (2) covered by the model
+    data_col_idx, model_col_idx = select_features(test_genes, test_assays, 
+                                                  model_genes, model_assays)
+
+    # Filter the data and model params to keep only those features
+    Z_test = Z_test[:,data_col_idx]
+    test_genes = test_genes[data_col_idx]
+    test_assays = test_assays[data_col_idx]
+
+    Y = Y[:,model_col_idx]
+    mu = mu[model_col_idx]
+    sigma = sigma[model_col_idx]
+    model_genes = model_genes[model_col_idx]
+    model_assays = model_assays[model_col_idx]
+
+    # Standardize the test data
+    Z_std = (Z_test - mu) / sigma
+
+    # Use the principal components to transform the data
+    # (have to handle missing data)
+    X = pca_transform(Z_std, Y)
+
+    # Save the transformed data to HDF
+    with h5py.File(out_hdf, "w") as f:
+        su.write_hdf(f, "X", X)
+        su.write_hdf(f, "instances", sample_ids, is_string=True) 
+        su.write_hdf(f, "instance_groups", sample_groups, is_string=True) 
