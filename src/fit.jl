1a96c72e82332cb0c6e117272665212448887958
dpmerrell <davidpmerrell@gmail.com>
2023-03-20 16:15:00 -0500

    Make kwargs for task-specific learning rates

--- src/fit.jl
+++ src/fit.jl
@@ -70,13 +70,14 @@ end
 
 
 function init_theta!(model::PathMatFacModel, opt; capacity=Int(10e8), max_epochs=500,
+                                                  lr_theta=0.001,
                                                   verbosity=1, print_prefix="", 
                                                   history=nothing, kwargs...)
     # Freeze everything except batch shift... 
     freeze_layer!(model.matfac.col_transform, 1:3)
 
     orig_lr = opt.eta 
-    opt.eta = 0.001
+    opt.eta = lr_theta
 
     h = mf_fit!(model; update_col_layers=true, capacity=capacity,
                        max_epochs=max_epochs, opt=opt,
@@ -364,7 +365,8 @@ function basic_fit!(model::PathMatFacModel; fit_batch=false,
                                             fit_joint=false,
                                             whiten=false,
                                             capacity=Int(10e8),
-                                            opt=nothing, lr=0.05, max_epochs=1000, 
+                                            opt=nothing, lr=0.05, max_epochs=1000,
+                                            lr_joint=0.001, 
                                             verbosity=1, print_prefix="",
                                             history=nothing,
                                             kwargs...)
@@ -448,7 +450,7 @@ function basic_fit!(model::PathMatFacModel; fit_batch=false,
         unfreeze_layer!(model.matfac.col_transform, 3:4) # batch and column shift 
         v_println("Jointly adjusting parameters..."; verbosity=verbosity, prefix=print_prefix)
         orig_lr = opt.eta
-        opt.eta = 0.001
+        opt.eta = lr_joint 
         h = mf_fit!(model; capacity=capacity, update_X=true, update_Y=true,
                                               update_col_layers=true,
                                               opt=opt,
@@ -629,7 +631,7 @@ end
 ############################################
 # Fit models with ARD regularization on Y
 
-function fit_ard!(model::PathMatFacModel; lr=0.05, opt=nothing, 
+function fit_ard!(model::PathMatFacModel; lr=0.05, ard_lr=0.005, opt=nothing, 
                                           verbosity=1, print_prefix="", 
                                           kwargs...)
 
@@ -657,7 +659,7 @@ function fit_ard!(model::PathMatFacModel; lr=0.05, opt=nothing,
                                            prefix=print_prefix)
     model.matfac.Y_reg = orig_ard
     orig_lr = opt.eta
-    opt.eta = 0.001
+    opt.eta = ard_lr
     basic_fit!(model; opt=opt, fit_factors=true, 
                                verbosity=verbosity,
                                print_prefix=n_pref,
